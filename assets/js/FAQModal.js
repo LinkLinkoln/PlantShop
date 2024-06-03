eb0248ebd680b51e62a7eddb7e702bdba75a64d1
Maxim <fresh88611@gmail.com>
2024-06-04 02:30:18 +0300

    3 часа ночи а я делаю перводик. Ещё и др, как здорово.

--- assets/js/FAQModal.js
+++ assets/js/FAQModal.js
@@ -24,84 +24,101 @@ const headerIcon = document.querySelector('.FAQInfo-image');
 const headerInfoH1 = document.querySelector('.FAQInfoH1');
 const headerInfoH2 = document.querySelector('.FAQInfoH2');
 const outputWindow = document.querySelector('.FAQSectionInformation');
+const FAQText = document.querySelector('.FAQ_text');
 const contentData = {
   CactusFaqContent: {
     SectionImage: 'assets/img/FAQ/cactus.png',
-    HeaderH1: 'Какие виды цветов вы продаёте?',
-    HeaderH2: 'Ознакомьтесь с нашим широким ассортиментом растений',
+    FAQMainText: `<div class="FAQInfoH1" data-i18="TypesOfFlowerText">
+    </div>
+    <div class="FAQInfoH2" data-i18="TypesOfFlowerMainText">
+    </div>
+    `,
     Code: `
     <div class="FlowersFAQ">
-    <div class="Flower" alt = "Rose">
-        <div class="FlowerFAQH1">
-            <a> Розы </a>
-        </div>
-        <div class="FAQDesignLine">
-        </div>
-        <div class="FlowerFAQText">
-            <a> Классика цветочного мира. У нас вы найдете розы различных 
-                расцветок, от нежных пастельных тонов до насыщенных 
-                красных. Мы поставляем розы как местного, так и 
-                импортного производства. </a>
-        </div>
+    <div class="Flower" alt="Rose">
+      <div class="FlowerFAQH1" alt="ChangableText" data-i18="RoseText">
+        <a> Розы </a>
+      </div>
+      <div class="FAQDesignLine"></div>
+      <div class="FlowerFAQText" data-i18="RoseMainText">
+          Классика цветочного мира. У нас вы найдете розы
+          различных расцветок, от нежных пастельных тонов до
+          насыщенных красных. Мы поставляем розы как местного, так
+          и импортного производства.
+      </div>
     </div>
-    <div class="Flower" alt = "Lilii">
-        <div class="FlowerFAQH1">
-            <a> Лилии </a>
-        </div>
-        <div class="FAQDesignLine" >
-        </div>
-        <div class="FlowerFAQText">
-            <a> Великолепные цветы с завораживающей формой и 
-                насыщенным ароматом. В продаже лилии Ориентальные, 
-                Азиатские, Трубчатые и другие разновидности. </a>
-        </div>
+    <div class="Flower" alt="Lilii">
+      <div class="FlowerFAQH1" data-i18="Liliy">
+        <a> Лилии </a>
+      </div>
+      <div class="FAQDesignLine"></div>
+      <div class="FlowerFAQText" data-i18="LiliyMainText" >
+        <a>
+          Великолепные цветы с завораживающей формой и насыщенным
+          ароматом. В продаже лилии Ориентальные, Азиатские,
+          Трубчатые и другие разновидности.
+        </a>
+      </div>
     </div>
-    <div class="Flower" alt = "Gwozdiki">
-        <div class="FlowerFAQH1">
-            <a> Гвоздики </a>
-        </div>
-        <div class="FAQDesignLine" >
-        </div>
-        <div class="FlowerFAQText">
-            <a> Яркие, долговечные цветы, широко используемые 
-                в букетах и композициях. Мы предлагаем классические 
-                гвоздики, а также необычные сорта с махровыми и крупными 
-                цветками. </a>
-        </div>
+    <div class="Flower" alt="Gwozdiki">
+      <div class="FlowerFAQH1" data-i18="GwozdikiText">
+        <a> Гвоздики </a>
+      </div>
+      <div class="FAQDesignLine"></div>
+      <div class="FlowerFAQText" data-i18="GwozdikiMainText">
+        <a>
+          Яркие, долговечные цветы, широко используемые в букетах
+          и композициях. Мы предлагаем классические гвоздики, а
+          также необычные сорта с махровыми и крупными цветками.
+        </a>
+      </div>
     </div>
-    <div class="Flower" alt = "Tulpanu">
-        <div class="FlowerFAQH1">
-            <a> Тюльпаны </a>
-        </div>
-        <div class="FAQDesignLine" >
-        </div>
-        <div class="FlowerFAQText">
-            <a> Весенние цветы с широкой палитрой цветов, от нежных 
-                пастельных до насыщенных ярких оттенков. </a>
-        </div>
+    <div class="Flower" alt="Tulpanu">
+      <div class="FlowerFAQH1" data-i18="TyplanText">
+        <a> Тюльпаны </a>
+      </div>
+      <div class="FAQDesignLine"></div>
+      <div class="FlowerFAQText" data-i18="TyplanMainText">
+        <a>
+          Весенние цветы с широкой палитрой цветов, от нежных
+          пастельных до насыщенных ярких оттенков.
+        </a>
+      </div>
     </div>
-</div>
+  </div>
     `
   },
   TimeFaqContent: {
     SectionImage: 'assets/img/FAQ/hourglass.png',
-    HeaderH1: 'Как долго будут свежими купленные цветы?',
-    HeaderH2: 'Узнайте сроки хранения наших растений',
+    FAQMainText: `
+    <div class="FAQInfoH1" data-i18="TimeText">
+    </div>
+    <div class="FAQInfoH2" data-i18="TimeMainText">
+    </div>
+    `,
     Code: `
     <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ad02e9532d33d20dfa605f98e3cb4488f69cb90d8985d3b9778658ba505fe9e0a&amp;width=539&amp;height=458&amp;lang=ru_RU&amp;scroll=true"></script>
     `
   },
   PackageFaqContent: {
     SectionImage: 'assets/img/FAQ/package.png',
-    HeaderH1: 'А что у нас интересного по доставке?',
-    HeaderH2: 'Узнайте о лучшей доставке страны',
+    FAQMainText: `
+    <div class="FAQInfoH1" data-i18="DeliveryFAQText">
+    </div>
+    <div class="FAQInfoH2" data-i18="DeliveryFAQMainText">
+    </div>
+    `,
     Code: '<h2>Информация для кнопки 3</h2>'
   },
   
   CreditCardFaqContent: {
     SectionImage: 'assets/img/FAQ/credit-card.png',
-    HeaderH1: 'Какие способы оплаты вы можете предложить?',
-    HeaderH2: 'Выберите удобный способ оплаты для вас',
+    FAQMainText: `
+    <div class="FAQInfoH1" data-i18="PaymentType">
+    </div>
+    <div class="FAQInfoH2" data-i18="PaymentMainType">
+    </div>
+    `,
     Code: '<h2>Информация для кнопки 4</h2>'
   }
 };
@@ -109,11 +126,12 @@ const contentData = {
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
+    switchLanguage();
     const contentKey = button.dataset.content;
     const content = contentData[contentKey];
     headerIcon.src = content.SectionImage;
-    headerInfoH1.textContent = content.HeaderH1
-    headerInfoH2.textContent = content.HeaderH2
+    FAQText.innerHTML = content.FAQMainText;
     outputWindow.innerHTML = content.Code;
+    switchLanguage();
   });
 });
--- assets/js/LanguageChange.js
+++ assets/js/LanguageChange.js
@@ -1,11 +1,65 @@
 const i18Obj = {
   en: {
-
+    sliderText1: "Lorem Ipsum is simply dummy text of the printing\nand typesetting industry. Lorem Ipsum has been the\nindustry's standard dummy text ever since the 1500s",
+    sliderItemDelivery: "Lorem Ipsum is simply dummy text of the printing \n and typesetting industry. Lorem Ipsum has been the \n industry's standard dummy text ever since the 1500s",
+    DeliveryText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+    CutstomerText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+    OriginalPlants: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+    AffordablePrice: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+    BuyMorePlantsText: "Buy more plants, it \n helps you to be relaxed",
+    BuyMorePlantsMainText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi \n gravida gravida aliquam. Pellentesque et lobortis nisl. Sed et \n mauris justo. Nulla eu enim non mauris maximus dignissim. \n Maecenas vitae eros sapien. Quisque pellentesque tempus \n dignissim.",
+    GetPlant: "Get your favourites plant on \n our shop now",
+    PreviousText: "Previous",
+    NextText: "Next",
+    RoseText: "Rose",
+    RoseMainText: "The classic of the floral world. At our store, you will find roses \n of various colors, from delicate pastel tones to \n rich reds. We supply roses from both local and \n imported production.",
+    Liliy: "Liliy",
+    LiliyMainText: "Magnificent flowers with a captivating form and rich aroma. We offer Oriental, Asiatic, Trumpet, and other varieties of lilies for sale.",
+    GwozdikiText: "Carnations",
+    GwozdikiMainText: "Vibrant, long-lasting flowers, widely used in bouquets and arrangements. We offer classic carnations as well as unique varieties with double and large blooms.",
+    TyplanText: "Tulips",
+    TyplanMainText: "Spring flowers with a wide palette of colors, from delicate pastels to rich, vibrant hues.",
+    TypesOfFlowerText: "What types of flowers do you sell?",
+    TypesOfFlowerMainText: "Check out our wide assortment of plants",
+    TimeText: "How long will the purchased flowers stay fresh?",
+    TimeMainText: "Learn about the storage life of our plants",
+    DeliveryFAQText: "What interesting do we have in delivery?",
+    DeliveryFAQMainText: "Learn about the best delivery in the country",
+    PaymentType: "What payment methods can you offer?",
+    PaymentMainType: "Choose the most convenient payment option for you",
   },
   ru: {
-
+    sliderText1: "Lorem Ipsum — это просто текст-пустышка полиграфической\nи наборной индустрии. Lorem Ipsum был стандартным\n текстом-пустышкой в ​​отрасли с 1500-х годов",
+    sliderItemDelivery: "Lorem Ipsum - это просто текст-заполнитель полиграфической и \n наборной промышленности. Lorem Ipsum используется с XVI века,\n став стандартным текстом-заполнителем в этой области.",
+    DeliveryText: "Lorem Ipsum - это просто текст-заполнитель полиграфической и наборной промышленности. Lorem Ipsum используется как стандартный текст-заполнитель в этой области",
+    CutstomerText: "Lorem Ipsum - это просто текст-заполнитель полиграфической и наборной промышленности. Lorem Ipsum используется как стандартный текст-заполнитель в этой области",
+    OriginalPlants: "Lorem Ipsum - это просто текст-заполнитель полиграфической и наборной промышленности. Lorem Ipsum используется как стандартный текст-заполнитель в этой области",
+    AffordablePrice: "Lorem Ipsum - это просто текст-заполнитель полиграфической и наборной промышленности. Lorem Ipsum используется как стандартный текст-заполнитель в этой области",
+    BuyMorePlantsText: "Покупайте больше растений, \n это помогает вам расслабиться",
+    BuyMorePlantsMainText: "Я без понятия как переводить этот бред...",
+    GetPlant: "Приобретите свои любимые растения \n в нашем магазине прямо сейчас",
+    PreviousText: "Предыдущий",
+    NextText: "Следующий",
+    RoseText: "Розы",
+    RoseMainText: "Классика цветочного мира. У нас вы найдете розы \n различных расцветок, от нежных пастельных тонов до \n насыщенных красных. Мы поставляем розы как местного, так \n и импортного производства.",
+    Liliy: "Лилии",
+    LiliyMainText: "Великолепные цветы с завораживающей формой и насыщенным ароматом. В продаже лилии Ориентальные, Азиатские, Трубчатые и другие разновидности.",
+    GwozdikiText: "Гвоздики",
+    GwozdikiMainText: "Яркие, долговечные цветы, широко используемые в букетах и композициях. Мы предлагаем классические гвоздики, а также необычные сорта с махровыми и крупными цветками.",
+    TyplanText: "Тюльпаны",
+    TyplanMainText: "Весенние цветы с широкой палитрой цветов, от нежных пастельных до насыщенных ярких оттенков.",
+    TypesOfFlowerText: "Какие виды цветов вы продаёте?",
+    TypesOfFlowerMainText: "Ознакомьтесь с нашим широким ассортиментом растений",
+    TimeText: "Как долго будут свежими купленные цветы?",
+    TimeMainText: "Узнайте сроки хранения наших растений",
+    DeliveryFAQText: "А что у нас интересного по доставке?",
+    DeliveryFAQMainText: "Узнайте о лучшей доставке страны",
+    PaymentType: "Какие способы оплаты вы можете преложить?",
+    PaymentMainType: "Выберете удобный способ оплаты для вас",
+ 
   }
-}
+};
+
 let currentLanguage = localStorage.getItem("language") || "en";
 
 function getTranslate(lang) {
@@ -22,43 +76,27 @@ function getTranslate(lang) {
   });
 }
 
-const enButton = document.querySelector('button[data-i18="en"]');
-const ruButton = document.querySelector('button[data-i18="rus"]');
+function switchLanguage() {
+  const languageButton = document.getElementById("ChangeLanguageBtn");
+  const languageIcon = languageButton.querySelector("img");
 
-enButton.addEventListener("click", () => {
-  getTranslate("en");
-  currentLanguage = "en";
-  localStorage.setItem("language", "en");
-});
-ruButton.addEventListener("click", () => {
-  getTranslate("ru");
-  currentLanguage = "ru";
-  localStorage.setItem("language", "ru");
-});
+  if (currentLanguage === "en") {
+    currentLanguage = "ru";
+    languageIcon.src = "assets/img/russia.png";
+  } else {
+    currentLanguage = "en";
+    languageIcon.src = "assets/img/united-kingdom.png";
+  }
 
-// Добавляем вызов функции при загрузке страницы
+  getTranslate(currentLanguage);
+  localStorage.setItem("language", currentLanguage);
+}
+
+const languageButton = document.getElementById("ChangeLanguageBtn");
+languageButton.addEventListener("click", switchLanguage);
+
 window.addEventListener("load", () => {
   getTranslate(currentLanguage);
+  const languageIcon = languageButton.querySelector("img");
+  languageIcon.src = currentLanguage === "en" ? "assets/img/united-kingdom.png" : "assets/img/russia.png";
 });
-if (
-  window.location.pathname !== "/html/registation.html" &&
-  window.location.pathname !== "/html/authorisation.html"
-) {
-  const enButtonB = document.querySelector('button[data-i18="enB"]');
-  const ruButtonB = document.querySelector('button[data-i18="rusB"]');
-
-  enButtonB.addEventListener("click", () => {
-    getTranslate("en");
-    enButtonB.classList.add("eng");
-    ruButtonB.classList.remove("russ");
-    currentLanguage = "en";
-    localStorage.setItem("language", "en");
-  });
-  ruButtonB.addEventListener("click", () => {
-    getTranslate("ru");
-    enButtonB.classList.remove("eng");
-    ruButtonB.classList.add("russ");
-    currentLanguage = "ru";
-    localStorage.setItem("language", "ru");
-  });
-}
