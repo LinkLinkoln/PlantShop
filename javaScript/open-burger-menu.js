b546b110880ed0e7ebfd0b50580e6fa96b859e43
Maxim <fresh88611@gmail.com>
2024-06-10 22:57:55 +0300

    Добавлено всплывающее окно.

--- javaScript/open-burger-menu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){
-  document.querySelector(".burger-menu-btn").addEventListener("click",function(){
-     const menu=document.querySelector(".burger-menu")
-     menu.classList.toggle("openBg");
-     document.body.classList.toggle("no-scroll",menu.classList.contains("openBg"));
-  })
-})
